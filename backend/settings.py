7691d7114e8d9ae0c0765ba40505ea8b04361b61
Safal <mr.safaladh@gmail.com>
2023-02-02 19:30:23 +0545

    Deploy

--- backend/settings.py
+++ backend/settings.py
@@ -165,7 +165,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR ,"media")
 # ]
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
